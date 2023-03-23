35a9373b18566fac656d77dc580bc5fad801b716
Ahmed Gamal <ahmed.gamal@brightskiesinc.com>
2023-03-23 07:37:30 +0200

    style(keycloak-00): code styling

--- src/views/greetings/greetings.ts
+++ src/views/greetings/greetings.ts
@@ -6,4 +6,6 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
     HelloWorld,
   },
 })
-export default class Greetings extends Vue {}
+export default class Greetings extends Vue {
+  message = 'Welcome to Your Vue.js + TypeScript App';
+}
--- src/views/greetings/greetings.vue
+++ src/views/greetings/greetings.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="home flex align-center gap-x-1">
-    <img alt="Vue logo" src="../../assets/logo.svg" height="128" width="128"/>
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="row">
+    <img alt="Vue logo" height="128" src="@/assets/logo.svg" width="128"/>
+    <HelloWorld :msg="message"/>
   </div>
 </template>
 
-<style scoped lang="scss" src="./greetings.scss"></style>
+<style lang="scss" scoped src="./greetings.scss"></style>
 <script lang="ts" src="./greetings.ts"></script>
