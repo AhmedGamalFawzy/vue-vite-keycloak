db1f1b0589951e32d5757efa5558bbbf51748bbd
Ahmed Gamal <ahmed.gamal@brightskiesinc.com>
2023-03-23 05:52:20 +0200

    vite-vue3-class-style-scss

---
+++ src/views/greetings/greetings.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home flex align-center gap-x-1">
+    <img alt="Vue logo" src="../../assets/logo.svg" height="128" width="128"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<style scoped lang="scss" src="./greetings.scss"></style>
+<script lang="ts" src="./greetings.ts"></script>
